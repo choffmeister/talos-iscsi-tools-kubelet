56a2825ecb039d4254c59767f4363a3345f2d0ba
Christian Hoffmeister <mail@choffmeister.de>
2022-06-11 07:12:32 +0200

    Handle iscsid pid edge cases
    Handle cases where there are no or more than one iscsid pid

--- rootfs/usr/local/sbin/iscsiadm-nsenter.sh
+++ rootfs/usr/local/sbin/iscsiadm-nsenter.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-iscsid_pid=$(pidof iscsid)
+iscsid_pid=$(pidof -s iscsid)
+if [ -z "${iscsid_pid}" ]; then
+  echo "Unable to find process id of iscsid"
+  exit 1
+fi
 nsenter --mount="/proc/${iscsid_pid}/ns/mnt" --net="/proc/${iscsid_pid}/ns/net" -- /usr/local/bin/iscsiadm "$@"
